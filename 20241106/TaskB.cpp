a7fa7b9b5cd42d5edc4b55876927188cf125ad9b
MotokiHashino <hashinomotoki@gmail.com>
2024-11-06 14:26:01 +0900

    TaskB完了。

--- 20241106/TaskB.cpp
+++ 20241106/TaskB.cpp
@@ -3,5 +3,15 @@
 
 int main()
 {
+    const int LOOP_COUNT = 10;
 
+    int i = 1;
+    while (i <= LOOP_COUNT)
+    {
+        std::cout << std::format("{}\n", i);
+        ++i;
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
