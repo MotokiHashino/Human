2d6e75cd74abb219d5395d325a4df2dc4718174e
MotokiHashino <hashinomotoki@gmail.com>
2024-11-13 11:48:23 +0900

    TaskD完了。

--- 20241106/TaskD.cpp
+++ 20241106/TaskD.cpp
@@ -3,5 +3,25 @@
 
 int main()
 {
+    const int LOOP_COUNT = 100;
+
+    for (int i = 1; i <= LOOP_COUNT; i++)
+    {
+        std::cout << std::format("{:3}:",i);
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            std::cout << std::format("fizz buzz");
+        }
+        else if (i % 3 == 0)
+        {
+            std::cout << std::format("fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            std::cout << std::format("buzz");
+        }
+        std::cout << std::endl;
+    }
+    
     return 0;
 }
