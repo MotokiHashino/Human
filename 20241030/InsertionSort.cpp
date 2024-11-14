819697b800e8de21d08df85d160dbdfe7ffb4aa5
MotokiHashino <hashinomotoki@gmail.com>
2024-11-14 12:02:44 +0900

    挿入ソート完成。

--- 20241030/InsertionSort.cpp
+++ 20241030/InsertionSort.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <format>
 
 int main()
 {
     //文字数
-    int const CHARACTER_QUANTITY = 12;
+    const int CHARACTER_QUANTITY = 12;
 
     //文字の代入、+1は終端のNUL文字用
     char characters[CHARACTER_QUANTITY + 1] = "HUMANACADEMY";
@@ -17,7 +18,43 @@ int main()
     }
     std::cout << std::endl;
 
+    //挿入ソート
+    for (int i = 0; i < CHARACTER_QUANTITY; ++i)
+    {
+        //挿入するindexの捜索
+        int insertionIndex = 0;
+        for (insertionIndex; insertionIndex < i; ++insertionIndex)
+        {
+            if (characters[i] < afterSortCharacters[insertionIndex])
+            {
+                break;
+            }
+
+        }
+
+        //配列の対応するindexに値を挿入
+        for (int j = i; j >= 0; --j)
+        {
+            if (j == insertionIndex)
+            {
+                afterSortCharacters[j] = characters[i];
+                break;
+            }
+            else
+            {
+                afterSortCharacters[j] = afterSortCharacters[j - 1];
+            }
+        }
+
+    }
+
+    //表示
+    for (int i = 0; i < CHARACTER_QUANTITY; ++i)
+    {
+        std::cout << afterSortCharacters[i];
+    }
+    std::cout << std::endl;
+
     return 0;
 
-    
 }
