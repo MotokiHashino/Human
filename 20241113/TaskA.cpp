e28eb7bdad15d99472e4763dab07645d68986846
MotokiHashino <hashinomotoki@gmail.com>
2024-11-27 13:32:14 +0900

    TaskA完了。

--- 20241113/TaskA.cpp
+++ 20241113/TaskA.cpp
@@ -3,5 +3,39 @@
 
 int main()
 {
+    int data[] = {0,1,2,3,4,5,6,7,8,9,10};
+    bool shuffledChecker[std::size(data)];
+    int shuffledData[std::size(data)];
+
+    srand((unsigned int)time(NULL));
+
+    for (int i = 0; i < std::size(data); ++i)
+    {
+        int index = rand() % std::size(data);
+        if (!shuffledChecker[index])
+        {
+            shuffledData[i] = data[index];
+            shuffledChecker[index] = true;
+        }
+        else
+        {
+            --i;
+        }
+    }
+
+    int result = -1;
+    int target = 5;
+
+    for (int i = 0; i < std::size(shuffledData); ++i)
+    {
+        if (target == shuffledData[i])
+        {
+            result = i;
+            break;
+        }
+    }
+
+    std::cout << std::format("targetのインデックスは{}です",result) << std::endl;
+
     return 0;
 }
