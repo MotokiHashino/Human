a29abda9ed180c4dcbd7ee88685cae2c0d79f0d9
MotokiHashino <hashinomotoki@gmail.com>
2024-12-09 17:52:24 +0900

    TaskB完了。

--- 20241113/TaskB.cpp
+++ 20241113/TaskB.cpp
@@ -3,5 +3,32 @@
 
 int main()
 {
+    const int DATA_SIZE = 100;
+    const int RAND_SIZE = 100;
+    int data[DATA_SIZE];
+
+    srand((unsigned int)time(NULL));
+
+    for (int i = 0; i < DATA_SIZE; ++i)
+    {
+        data[i] = rand() % RAND_SIZE;
+    }
+
+    for (int i = 0; i < DATA_SIZE; ++i)
+    {
+        for (int j = DATA_SIZE - 1; j >= i + 1; --j)
+        {
+            int valueA = data[i];
+            int valueB = data[j];
+            if (valueA > valueB)
+            {
+                int max = valueA;
+                int min = valueB;
+                data[i] = min;
+                data[j] = max;
+            }
+        }
+    }
+
     return 0;
 }
