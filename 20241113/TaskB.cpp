3df2e77be967d6ba3624013cd5737f9d8254b8ee
MotokiHashino <hashinomotoki@gmail.com>
2024-12-10 09:03:57 +0900

    TaskB修正。

--- 20241113/TaskB.cpp
+++ 20241113/TaskB.cpp
@@ -3,17 +3,19 @@
 
 int main()
 {
-    const int DATA_SIZE = 100;
-    const int RAND_SIZE = 100;
-    int data[DATA_SIZE];
+    const int DATA_SIZE = 100;//配列の数
+    const int RAND_SIZE = 100;//乱数の幅
+    int data[DATA_SIZE];//配列宣言
 
-    srand((unsigned int)time(NULL));
+    srand((unsigned int)time(NULL));//乱数シャッフル
 
+    //乱数代入
     for (int i = 0; i < DATA_SIZE; ++i)
     {
         data[i] = rand() % RAND_SIZE;
     }
 
+    //ソート
     for (int i = 0; i < DATA_SIZE; ++i)
     {
         for (int j = DATA_SIZE - 1; j >= i + 1; --j)
@@ -30,5 +32,10 @@ int main()
         }
     }
 
+    //出力
+    for (int i = 0; i < DATA_SIZE; ++i)
+    {
+        std::cout << data[i] << std::endl;
+    }
     return 0;
 }
